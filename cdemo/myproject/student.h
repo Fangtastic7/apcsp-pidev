5ffb2926f021e48b991930359ab688425aab6d04
Fangtastic7 <30578148+Fangtastic7@users.noreply.github.com>
2019-05-29 18:52:44 -0700

    Update student.h

--- cdemo/myproject/student.h
+++ cdemo/myproject/student.h
@@ -1,1 +1,11 @@
 
+//student data structure
+struct Student {
+  char fname[50];
+  char lname[50];
+  int age;
+  int id;
+};
+//declare methods
+int enterStudent (struct Student* student);
+int printStudent (struct Student* student); 
