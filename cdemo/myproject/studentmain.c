4880276aebbe712a72f7f30da1709b57b3fb25f8
Fangtastic7 <30578148+Fangtastic7@users.noreply.github.com>
2019-05-29 18:53:13 -0700

    Update studentmain.c

--- cdemo/myproject/studentmain.c
+++ cdemo/myproject/studentmain.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <string.h>
+#include "student.h" 
 
+int main ()
+{
+	char cont[50];
+	int i = 0;
+	int n = 0;
+
+	struct Student myStudent[25];
+	do {
+	enterStudent(&myStudent[i]);
+	i++;
+	printf("Add another? (Y/N) ");
+	fgets(cont, 50, stdin);
+} while (i < 25 && (cont[0] == 'Y'));
+
+printf("\n");
+for (n = 0; n < i; n++) 
+	printStudent(&myStudent[n]);
+}
